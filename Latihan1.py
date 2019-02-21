882c2252b78251ed326439ca9c996bac9128beca
bagusdwisaputro <47028610+bagusdwisaputro@users.noreply.github.com>
2019-02-22 03:43:45 +0700

    Add files via upload

---
+++ Latihan1.py
@@ -0,0 +1,5 @@
+jumlah =int(input("Masukkan jumlah N :"))
+import random
+for i in range (jumlah):
+    print("Data ke", i+1,"=",(random.uniform(0.1,0.5)))
+
---
+++ program1.py
@@ -0,0 +1,19 @@
+x=100000000
+print ("Investasi modal awal :",x)
+a=0*x
+b=0*x
+c=0.01*x
+d=0.01*x
+e=0.05*x
+f=0.05*x
+g=0.05*x
+h=0.03*x
+y=[a,b,c,d,e,f,g,h]
+
+for i in range (len(y)):
+    print ("Laba bulan ke",i+1  ,"Sebesar :",y[i])
+
+z= (a+b+c+d+e+f+g+h)
+print ("\nJumlah Laba selama 8 bulan adalah :",z)
+
+
